0e54a70c635a27f03713f2ffdce408b923214eb8
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-07-30 21:03:52 +0200

    All buttons work properly by updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
   const [store, setStore] = useState([
     {
       id: 1,
-      name: 'beetroot',
+      name: "beetroot",
       price: 0.45,
       stock: 10,
-      inCart: 0,
+      inCart: 7,
     },
     {
       id: 2,
-      name: 'carrot',
+      name: "carrot",
       price: 0.15,
-      stock: 2,
+      stock: 8,
       inCart: 5,
     },
     {
       id: 3,
-      name: 'apple',
+      name: "apple",
       price: 0.25,
+      stock: 1,
+      inCart: 3,
+    },
+    {
+      id: 4,
+      name: "apricot",
+      price: 0.8,
+      stock: 4,
+      inCart: 3,
+    },
+    {
+      id: 5,
+      name: "avocado",
+      price: 0.9,
+      stock: 5,
+      inCart: 0,
+    },
+    {
+      id: 6,
+      name: "bananas",
+      price: 0.6,
+      stock: 5,
+      inCart: 0,
+    },
+    {
+      id: 7,
+      name: "bell-pepper",
+      price: 0.33,
       stock: 1,
       inCart: 0,
     },
-  ])
+    {
+      id: 8,
+      name: "berry",
+      price: 0.5,
+      stock: 5,
+      inCart: 0,
+    },
+    {
+      id: 9,
+      name: "blueberry",
+      price: 0.5,
+      stock: 1,
+      inCart: 0,
+    },
+    {
+      id: 10,
+      name: "eggplant",
+      price: 0.75,
+      stock: 1,
+      inCart: 0,
+    },
+  ]);
+
+  const cart = getCartItems();
 
   function getItemImagePath(item: any) {
-    let id = String(item.id).padStart(3, '0')
-    return `assets/icons/${id}-${item.name}.svg`
+    let id = String(item.id).padStart(3, "0");
+    return `assets/icons/${id}-${item.name}.svg`;
   }
 
   function getCartItems() {
-    return store.filter(item => item.inCart > 0)
+    return store.filter((item) => item.inCart > 0);
   }
 
+  function getTotal() {
+    let total = 0;
+    for (let item of cart) {
+      total += item.inCart * item.price;
+    }
+    return total;
+  }
+
+  function increaseCartQuantity(item) {
+    if (item.stock === 0) return;
+    const storeCopy = structuredClone(store);
+    const match = storeCopy.find((target) => target.id === item.id);
+    match.inCart++;
+    match.stock--;
+    setStore(storeCopy);
+  }
+
+  function decreaseCartQuantity(item) {
+    if (item.inCart < 1) return;
+    const storeCopy = structuredClone(store);
+    const match = storeCopy.find((target) => target.id === item.id);
+    match.inCart--;
+    match.stock++;
+    setStore(storeCopy);
+  }
 
   return (
     <div className="App">
       <header id="store">
         <h1>Grocero</h1>
         <ul className="item-list store--item-list">
-
-          {
-            store.map(item => (
-              <li>
-                <div className=".store--item-icon">
-                  <img src="assets/icons/001-beetroot.svg" />
-                </div>
-                <button>Add to cart (10)</button>
-              </li>))
-          }
-
+          {store.map((item) => (
+            <li>
+              <div className=".store--item-icon">
+                <img src={getItemImagePath(item)} />
+              </div>
+              <button
+                onClick={function () {
+                  increaseCartQuantity(item);
+                }}
+              >
+                Add to cart ({item.stock})
+              </button>
+            </li>
+          ))}
         </ul>
       </header>
 
@@ -61,13 +140,33 @@ function App() {
 
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            <li>
-              <img className="cart--item-icon" src="assets/icons/002-carrot.svg" alt="carrot" />
-              <p>carrot</p>
-              <button className="quantity-btn remove-btn center">-</button>
-              <span className="quantity-text center">5</span>
-              <button className="quantity-btn add-btn center">+</button>
-            </li>
+            {cart.map((item) => (
+              <li>
+                <img
+                  className="cart--item-icon"
+                  src={getItemImagePath(item)}
+                  alt={item.name}
+                />
+                <p>{item.name}</p>
+                <button
+                  className="quantity-btn remove-btn center"
+                  onClick={function () {
+                    decreaseCartQuantity(item);
+                  }}
+                >
+                  -
+                </button>
+                <span className="quantity-text center">{item.inCart}</span>
+                <button
+                  className="quantity-btn add-btn center"
+                  onClick={function () {
+                    increaseCartQuantity(item);
+                  }}
+                >
+                  +
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -77,12 +176,12 @@ function App() {
           </div>
 
           <div>
-            <span className="total-number">£0.00</span>
+            <span className="total-number">{getTotal().toFixed(2)} €</span>
           </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
